50ab37a0eb2cc08c47142e98588425782970cbaf
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-04-20 10:03:31 +0700

    feat: Update Major

--- obj/Debug/net8.0/ilcs-pelindo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ilcs-pelindo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ilcs-pelindo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec21ac5bba0262da75f9f9d50a1f8ddb24d0112")]
 [assembly: System.Reflection.AssemblyProductAttribute("ilcs-pelindo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ilcs-pelindo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
